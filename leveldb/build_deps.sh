0c336c59bf72b8795212abdfb05cee06f4e0a476
siddontang <siddontang@gmail.com>
2014-04-21 16:24:56 +0800

    add snappy and leveldb to LD_LIBRARY_PATH

--- leveldb/build_deps.sh
+++ leveldb/build_deps.sh
@@ -35,7 +35,21 @@ else
     echo "skip install leveldb"
 fi
 
+function add_path()
+{
+  # $1 path variable
+  # $2 path to add
+  if [ -d "$2" ] && [[ ":$1:" != *":$2:"* ]]; then
+    echo "$1:$2"
+  else
+    echo "$1"
+  fi
+}
+
 export CGO_CFLAGS="-I$LEVELDB_DIR/include -I$SNAPPY_DIR/include"
 export CGO_LDFLAGS="-L$LEVELDB_DIR/lib -L$SNAPPY_DIR/lib -lsnappy"
+export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $SNAPPY_DIR/lib)
+export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $LEVELDB_DIR/lib)
+
 
 go get github.com/jmhodges/levigo 
