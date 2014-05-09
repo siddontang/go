f02308217faca083aee4a739ae7a33c391fabd29
siddontang <siddontang@gmail.com>
2014-05-09 09:39:23 +0800

    hack add int to slice

--- hack/hack_test.go
+++ hack/hack_test.go
@@ -2,6 +2,7 @@ package hack
 
 import (
 	"bytes"
+	"encoding/binary"
 	"testing"
 )
 
@@ -32,5 +33,23 @@ func TestByte(t *testing.T) {
 
 	if !bytes.Equal(b, []byte("hello world")) {
 		t.Fatal(string(b))
+	}
+}
+
+func TestInt(t *testing.T) {
+	if int64(binary.LittleEndian.Uint64(IntSlice(1))) != 1 {
+		t.Fatal("error")
+	}
+
+	if int64(binary.LittleEndian.Uint64(IntSlice(-1))) != -1 {
+		t.Fatal("error")
+	}
+
+	if int64(binary.LittleEndian.Uint64(IntSlice(32768))) != 32768 {
+		t.Fatal(1)
+	}
+
+	if int64(binary.LittleEndian.Uint64(IntSlice(-32768))) != -32768 {
+		t.Fatal(1)
 	}
 }
