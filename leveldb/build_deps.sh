3dfdf380f2bd0cbb3cfc0d8a7e66bfcd9ef558fd
siddontang <siddontang@gmail.com>
2014-05-14 09:02:10 +0800

    update leveled build_deps.sh

--- leveldb/build_deps.sh
+++ leveldb/build_deps.sh
@@ -2,19 +2,16 @@
 
 #refer https://github.com/norton/lets/blob/master/c_src/build_deps.sh
 
-#install leveldb and snappy
-
-#you must set your own snappy and leveldb source directory
-#snappy https://drive.google.com/file/d/0B0xs9kK-b5nMOWIxWGJhMXd6aGs/edit?usp=sharing
-#leveldb https://leveldb.googlecode.com/files/leveldb-1.15.0.tar.gz
-SNAPPY_SRC=./snappy
-LEVELDB_SRC=./leveldb
-
 SNAPPY_DIR=/usr/local/snappy
 LEVELDB_DIR=/usr/local/leveldb
 
+mkdir -p ./build
+
+cd ./build
+
 if [ ! -f $SNAPPY_DIR/lib/libsnappy.a ]; then
-    (cd $SNAPPY_SRC && \
+    (git clone git@github.com:siddontang/snappy.git && \
+        cd ./snappy && \
         ./configure --prefix=$SNAPPY_DIR && \
         make && \
         make install)
@@ -23,7 +20,8 @@ else
 fi
 
 if [ ! -f $LEVELDB_DIR/lib/libleveldb.a ]; then
-    (cd $LEVELDB_SRC && \
+    (git clone git@github.com:siddontang/leveldb.git && \
+        cd ./leveldb && \
         echo "echo \"PLATFORM_CFLAGS+=-I$SNAPPY_DIR/include\" >> build_config.mk" >> build_detect_platform &&
         echo "echo \"PLATFORM_CXXFLAGS+=-I$SNAPPY_DIR/include\" >> build_config.mk" >> build_detect_platform &&
         echo "echo \"PLATFORM_LDFLAGS+=-L $SNAPPY_DIR/lib -lsnappy\" >> build_config.mk" >> build_detect_platform &&
@@ -37,18 +35,8 @@ else
     echo "skip install leveldb"
 fi
 
-function add_path()
-{
-  # $1 path variable
-  # $2 path to add
-  if [ -d "$2" ] && [[ ":$1:" != *":$2:"* ]]; then
-    echo "$1:$2"
-  else
-    echo "$1"
-  fi
-}
-
 export CGO_CFLAGS="-I$LEVELDB_DIR/include -I$SNAPPY_DIR/include"
+export CGO_CXXFLAGS="-I$LEVELDB_DIR/include -I$SNAPPY_DIR/include"
 export CGO_LDFLAGS="-L$LEVELDB_DIR/lib -L$SNAPPY_DIR/lib -lsnappy"
 export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $SNAPPY_DIR/lib)
 export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $LEVELDB_DIR/lib)
