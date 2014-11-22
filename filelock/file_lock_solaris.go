8f64946c30746240c2f3bdb606eed9a4aca34478
siddontang <siddontang@gmail.com>
2014-11-22 08:47:30 +0800

    file lock use flock

--- filelock/file_lock_solaris.go
+++ filelock/file_lock_solaris.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd linux netbsd openbsd
+// +build solaris
 
 package filelock
 
@@ -27,25 +27,17 @@ func Lock(name string) (io.Closer, error) {
 		return nil, err
 	}
 
-	/*
-		Some people tell me FcntlFlock does not exist, so use flock here
-	*/
-	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+	spec := syscall.Flock_t{
+		Type:   syscall.F_WRLCK,
+		Whence: int16(os.SEEK_SET),
+		Start:  0,
+		Len:    0, // 0 means to lock the entire file.
+		Pid:    int32(os.Getpid()),
+	}
+	if err := syscall.FcntlFlock(f.Fd(), syscall.F_SETLK, &spec); err != nil {
 		f.Close()
 		return nil, err
 	}
-
-	// spec := syscall.Flock_t{
-	// 	Type:   syscall.F_WRLCK,
-	// 	Whence: int16(os.SEEK_SET),
-	// 	Start:  0,
-	// 	Len:    0, // 0 means to lock the entire file.
-	// 	Pid:    int32(os.Getpid()),
-	// }
-	// if err := syscall.FcntlFlock(f.Fd(), syscall.F_SETLK, &spec); err != nil {
-	// 	f.Close()
-	// 	return nil, err
-	// }
 
 	return lockCloser{f}, nil
 }
